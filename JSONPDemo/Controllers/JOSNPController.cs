b1f0c43d850282752f990b9ae367c41b29a08207
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-07-30 22:52:09 +0530

    Added JSONP Formatter

---
+++ JSONPDemo/Controllers/JOSNPController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace JSONPDemo.Controllers
+{
+    public class JOSNPController:ApiController
+    {
+       
+        public string Get(int id)
+        {
+            return "demo";
+        }
+
+        public HttpResponseMessage Get(int id,string callBack)
+        {
+            var content = new JOSNPReturn
+            {
+                CallBack = callBack,
+                JSON = "{'id':'"+id.ToString()+"','data':'Hello from jsonpp controller'}"
+            };
+
+            var msg = Request.CreateResponse(HttpStatusCode.OK,content,"application/javascript");
+            return msg;
+        }
+    }
+}
---
+++ JSONPDemo/Controllers/JOSNPReturn.cs
@@ -0,0 +1,8 @@
+﻿namespace JSONPDemo.Controllers
+{
+    public class JOSNPReturn
+    {
+        public string CallBack { get; internal set; }
+        public string JSON { get; internal set; }
+    }
+}
