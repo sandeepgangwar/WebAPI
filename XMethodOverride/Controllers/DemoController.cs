724e006cf94177033d716f2fb8b9e738176eae34
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-07-30 23:46:14 +0530

    Added Override method project

---
+++ XMethodOverride/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace XMethodOverride.Controllers
+{
+    public class DemoController:ApiController
+    {
+        public void Get()
+        {
+
+        }
+
+        public void Post()
+        {
+
+        }
+
+        public void Put()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ XMethodOverride/MyMethodOverrideHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XMethodOverride
+{
+    internal class MyMethodOverrideHandler : DelegatingHandler
+    {
+        const string header = "X-HTTP-Method-Override";
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if(request.Method == HttpMethod.Post &&
+                request.Headers.Contains(header))
+            {
+                var realVerb = request.Headers.GetValues(header).FirstOrDefault();
+                if(realVerb != null)
+                {
+                    request.Method = new HttpMethod(realVerb);
+                }
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
