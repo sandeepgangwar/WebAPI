a7093337fef91d2ded73ac9d601ac465daf0f323
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-07-30 15:23:29 +0530

    Added Video upload client

---
+++ UploadVideoClient.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadVideoClient.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var stream = File.Open("small.mp4", FileMode.Open);
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(stream),@"\video\",@"\video.mp4\");
+            var res = client.PostAsync("", content).ContinueWith((r) =>
+            {
+                r.Result.EnsureSuccessStatusCode();
+            });
+        }
+    }
+}
