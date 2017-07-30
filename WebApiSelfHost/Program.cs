ee5b1b7bd4c5d9910d96a868b493391782d1f522
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-07-30 16:18:05 +0530

    Added self hoted project

---
+++ WebApiSelfHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Hosting;
+using System.Web.Http.SelfHost;
+namespace WebApiSelfHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8089");
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server running");
+            Console.ReadLine();
+        }
+    }
+}
